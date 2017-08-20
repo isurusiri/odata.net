d57e96991b1d4d1534f73d971d73caf32d98fb19
isurusiri <isuruanatomy@gmail.com>
2017-08-20 08:50:41 +0530

    initial src

---
+++ ProductsApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp
+{
+    class Program
+    {
+        static void ListAllProducts(Default.Container container)
+        {
+            foreach (var p in container.Products)
+            {
+                Console.WriteLine("{0} {1} {2}", p.Name, p.Price, p.Category);
+            }
+        }
+
+        static void AddProduct(Default.Container container, ProductService.Models.Product product)
+        {
+            container.AddToProducts(product);
+            var serviceResponse = container.SaveChanges();
+            foreach(var operationResponse in serviceResponse)
+            {
+                Console.WriteLine("Response: {0}", operationResponse.StatusCode);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string serviceUri = "http://localhost:56325/";
+            var container = new Default.Container(new Uri(serviceUri));
+
+            var product = new ProductService.Models.Product()
+            {
+                Name = "Yo-yo",
+                Category = "Toys",
+                Price = 4.95M
+            };
+
+            AddProduct(container, product);
+            ListAllProducts(container);
+
+            Console.ReadKey();
+        }
+    }
+}
